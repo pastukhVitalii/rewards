b607d4f4bd80b793a136ec4c1f2e024fb0fb0519
pastukhVitalii <pastukh.v.7@gmail.com>
2020-12-17 20:01:12 +0200

    added firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import firebase from "firebase/app";
+import 'firebase/database';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    useEffect(() => {
+        const db = firebase.database();
+        console.log(db)
+    })
+
+    return (
+        <div className="App">
+
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBgCes5l0pxzkp9wXByw0x8v0gVKt5A0bo",
+    authDomain: "rewards-542d1.firebaseapp.com",
+    databaseURL: "https://rewards-542d1-default-rtdb.firebaseio.com",
+    projectId: "rewards-542d1",
+    storageBucket: "rewards-542d1.appspot.com",
+    messagingSenderId: "290727625693",
+    appId: "1:290727625693:web:2fc25609c9ab6bbdef6c34"
+}
+
+firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(
   <React.StrictMode>
